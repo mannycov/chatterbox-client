436b932aef93f9cb2959ccf1d59c91170af7891b
mannycov <manny.cov09@gmail.com>
2017-08-12 16:54:44 -0700

    added fetch method

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -11,16 +11,35 @@ var app = {
   send: function() {
     $.ajax({
 
-      url: 'http://parse.SFM.hackreactor.com/chatterbox/classes/messages',
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
       type: 'POST',
       dataType: 'jsonp',
-      success: function(messageObj) {
-        return messageObj.username + ': ' + messageObj.text;
+      success: function (data) {
+        return data.text;
+        console.log('chatterbox: Message sent');
+      },
+
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
       }
+
 
     });
 
   },
+
+  fetch: function() {
+    $.ajax({
+
+      url: undefined,
+      type: 'GET',
+      dataType: 'jsonp',
+
+
+    });
+
+  }
 
 
 
